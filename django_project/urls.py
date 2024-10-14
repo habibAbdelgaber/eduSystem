edfa1ddbb8a8e83f45dab3674da755ec4ec7aa8c
Pymastery <47949285+habibAbdelgaber@users.noreply.github.com>
2024-10-14 17:08:27 +0000

    Django debug toolbar #4

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-# from debug_toolbar.toolbar import debug_toolbar_urls
-import debug_toolbar
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
@@ -12,7 +10,7 @@ urlpatterns = [
     path('',
          TemplateView.as_view(template_name='core/index.html'),
          name='index'),
-    path('__debug__/', include(debug_toolbar.urls))
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 if settings.DEBUG:
@@ -21,7 +19,7 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
 
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    # import debug_toolbar
+    # urlpatterns = [
+    #     path('__debug__/', include(debug_toolbar.urls)),
+    # ] + urlpatterns
