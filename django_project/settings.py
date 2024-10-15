f8fd28324b41d7af03c84d089b5c4d9ea28eb3ae
Pymastery <47949285+habibAbdelgaber@users.noreply.github.com>
2024-10-15 12:02:40 +0300

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,7 +20,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
