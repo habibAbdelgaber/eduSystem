fcdb9e6db4bef02f8748e034cc5728796edf37b8
Pymastery <47949285+habibAbdelgaber@users.noreply.github.com>
2024-10-17 17:07:50 +0300

    Update tests.py

--- core/tests.py
+++ core/tests.py
@@ -2,7 +2,7 @@
 Core tests
 """
 
-from django.conf import settings
+# from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
